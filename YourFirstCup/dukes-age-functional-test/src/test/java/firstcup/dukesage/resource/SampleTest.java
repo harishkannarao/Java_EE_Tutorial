60b5d317c336496f323c5ed4951f312d6a891ce7
harishkannarao <harish.gtec@gmail.com>
2015-01-09 17:58:25 +0000

    Using Maven invoker plugin

--- YourFirstCup/dukes-age-functional-test/src/test/java/firstcup/dukesage/resource/SampleTest.java
+++ YourFirstCup/dukes-age-functional-test/src/test/java/firstcup/dukesage/resource/SampleTest.java
@@ -1,24 +1,36 @@
 package firstcup.dukesage.resource;
 
+import org.apache.maven.shared.invoker.*;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Collections;
 
 import static org.junit.Assert.assertTrue;
 
 public class SampleTest {
 
     @Test
-    public void testServerStartup() {
+    public void testServerStartup() throws Exception {
         boolean serverRunning = ping("http://localhost:8080", 1000);
 
-        assertTrue(serverRunning);
+        if(!serverRunning) {
+            deployApps();
+//            startServer();
 
+            assertTrue(ping("http://localhost:8080", 5000));
+        }
     }
 
-    public static boolean ping(String url, int timeoutInMillis) {
+    @Test
+    public void simpleTest() {
+        System.out.print(new File("../pom.xml").getAbsolutePath());
+    }
+
+    public boolean ping(String url, int timeoutInMillis) {
         try {
             HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
             connection.setConnectTimeout(timeoutInMillis);
@@ -28,6 +40,39 @@ public class SampleTest {
             return (200 == responseCode);
         } catch (IOException exception) {
             return false;
+        }
+    }
+
+    public void startServer() throws Exception {
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                InvocationRequest request = new DefaultInvocationRequest();
+                request.setPomFile(new File("../wildfly-setup/pom.xml"));
+                request.setGoals(Collections.singletonList("cargo:run"));
+
+                Invoker invoker = new DefaultInvoker();
+                try {
+                    invoker.execute( request );
+                } catch (MavenInvocationException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t1.start();
+
+    }
+
+    public void deployApps() throws Exception {
+        InvocationRequest request = new DefaultInvocationRequest();
+        request.setPomFile( new File( "../pom.xml" ) );
+        request.setGoals( Collections.singletonList("install") );
+        request.setProfiles(Collections.singletonList("deploy-apps"));
+
+        Invoker invoker = new DefaultInvoker();
+        InvocationResult result = invoker.execute( request );
+
+        if ( result.getExitCode() != 0 ) {
+            throw new IllegalStateException( "Build failed. Please run 'mvn clean install -P deploy-apps' to know the reason" );
         }
     }
 }
