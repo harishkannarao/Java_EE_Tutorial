34d1075b847c96ea14dde10a3b28aac9e4c280a0
harishkannarao <harish.gtec@gmail.com>
2015-01-28 17:20:08 +0000

    Starting with Page Object pattern for web driver

---
+++ YourFirstCup/firstcup-war-functional-test/src/test/java/firstcup/page/HomePage.java
@@ -0,0 +1,13 @@
+package firstcup.page;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    private final WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
