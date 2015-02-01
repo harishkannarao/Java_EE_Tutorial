d92bc6f6dc2ab84ad6b189596c6b58a3acafd363
Harish Kannarao <harish.gtec@gmail.com>
2015-02-01 19:50:21 +0000

    Enabled CDI Weld in the functional test

--- YourFirstCup/firstcup-war-functional-test/src/test/java/firstcup/test/TestBase.java
+++ YourFirstCup/firstcup-war-functional-test/src/test/java/firstcup/test/TestBase.java
@@ -1,5 +1,6 @@
 package firstcup.test;
 
+import org.jboss.weld.environment.se.Weld;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
@@ -11,6 +12,8 @@ public abstract class TestBase {
     // Notice that the remainder of the code relies on the interface,
     // not the implementation.
     private static WebDriver driver = null;
+    
+    private static Weld weld = null;
 
     protected String getAppServerUrl() {
         return System.getProperty("appServerUrl", "http://localhost:8080");
@@ -28,6 +31,17 @@ public abstract class TestBase {
     @AfterClass
     public static void closeWebDriver() {
         driver.quit();
+    }
+    
+    @BeforeClass
+    public static void startWeldContainer() {
+        weld = new Weld();
+        weld.initialize();
+    }
+    
+    @AfterClass
+    public static void stopWeldContainer() {
+        weld.shutdown();
     }
 
 }
