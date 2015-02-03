1f4d1d77ccaccb8081bd11a71b7e1af2dbfc25d5
Harish Kannarao <harish.gtec@gmail.com>
2015-02-03 13:37:46 +0000

    Using WeldContext singleton instance

--- YourFirstCup/firstcup-war-functional-test/src/test/java/firstcup/runner/WeldJUnit4Runner.java
+++ YourFirstCup/firstcup-war-functional-test/src/test/java/firstcup/runner/WeldJUnit4Runner.java
@@ -6,20 +6,13 @@ import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
 
 public class WeldJUnit4Runner extends BlockJUnit4ClassRunner {
-    private final Class<?> klass;
-    private final Weld weld;
-    private final WeldContainer container;
-
-    public WeldJUnit4Runner(final Class<Object> klass) throws InitializationError {
-        super(klass);
-        this.klass = klass;
-        this.weld = new Weld();
-        this.container = weld.initialize();
+    public WeldJUnit4Runner(Class<Object> clazz) throws InitializationError {
+        super(clazz);
     }
 
     @Override
-    protected Object createTest() throws Exception {
-        final Object test = container.instance().select(klass).get();
-        return test;
+    protected Object createTest() {
+        final Class<?> test = getTestClass().getJavaClass();
+        return WeldContext.INSTANCE.getBean(test);
     }
 }
