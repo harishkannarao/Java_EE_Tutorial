d4d2634221c0abd8dcd8866dfbd5c78e05002581
Harish Kannarao <harish.gtec@gmail.com>
2015-02-13 15:17:09 +0000

    Fixed the home page features

--- YourFirstCup/firstcup-war-bdd-functional-test/src/test/java/firstcup/bdd/feature/home/HomeStepDefinitions.java
+++ YourFirstCup/firstcup-war-bdd-functional-test/src/test/java/firstcup/bdd/feature/home/HomeStepDefinitions.java
@@ -1,7 +1,7 @@
 package firstcup.bdd.feature.home;
 
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 import firstcup.page.GreetingPage;
 import firstcup.page.HomePage;
 
@@ -15,13 +15,13 @@ public class HomeStepDefinitions {
     HomePage homePage;
     @Inject
     GreetingPage greetingPage;
-    
-    @When("^: I navigate to home page$")
+
+    @Given("^I navigate to home page$")
     public void i_navigate_to_home_page() throws Throwable {
         homePage.navigate();
     }
 
-    @Then("^: I should see greeting page$")
+    @Then("^I should see greeting page$")
     public void i_should_see_greeting_page() throws Throwable {
         assertTrue(greetingPage.isOnValidPage());
     }
--- YourFirstCup/firstcup-war-bdd-functional-test/src/test/resources/firstcup/bdd/feature/home/home.feature
+++ YourFirstCup/firstcup-war-bdd-functional-test/src/test/resources/firstcup/bdd/feature/home/home.feature
@@ -1,5 +1,5 @@
 Feature: Home Page
   
   Scenario: should display greeting page as home page
-    When : I navigate to home page
-    Then : I should see greeting page
+    Given I navigate to home page
+    Then I should see greeting page
