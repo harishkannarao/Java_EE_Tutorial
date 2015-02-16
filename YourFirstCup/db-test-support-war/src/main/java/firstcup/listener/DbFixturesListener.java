2eaa99ca6fd454b3888ecf7a974e122a0832a66a
Harish Kannarao <harish.gtec@gmail.com>
2015-02-16 17:35:51 +0000

    Added h2 console to test support war

---
+++ YourFirstCup/db-test-support-war/src/main/java/firstcup/listener/DbFixturesListener.java
@@ -0,0 +1,29 @@
+package firstcup.listener;
+
+import firstcup.sql.util.DbFixtures;
+
+import javax.inject.Inject;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.util.logging.Logger;
+
+public class DbFixturesListener implements ServletContextListener {
+    private final static Logger LOGGER = Logger.getLogger(DbFixturesListener.class.getName());
+    @Inject
+    private DbFixtures dbFixtures;
+    
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        /*try {
+            dbFixtures.resetDbFixtures();
+        } catch (Exception e) {
+            LOGGER.severe(e.getMessage());
+            throw new RuntimeException(e);
+        }*/
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
