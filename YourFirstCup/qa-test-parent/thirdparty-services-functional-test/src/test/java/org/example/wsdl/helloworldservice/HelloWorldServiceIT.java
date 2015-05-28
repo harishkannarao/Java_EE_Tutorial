e60383e592256334929ac6d58c8585eb3c280483
Harish Kannarao <harish.gtec@gmail.com>
2015-05-28 22:36:43 +0100

    Implemented a thirdparty webservice test

--- YourFirstCup/qa-test-parent/thirdparty-services-functional-test/src/test/java/org/example/wsdl/helloworldservice/HelloWorldServiceIT.java
+++ YourFirstCup/qa-test-parent/thirdparty-services-functional-test/src/test/java/org/example/wsdl/helloworldservice/HelloWorldServiceIT.java
@@ -1,8 +1,39 @@
 package org.example.wsdl.helloworldservice;
 
-import javax.xml.ws.WebServiceRef;
+import org.example.xsd.sayhello.SayHelloRequestType;
+import org.example.xsd.sayhello.SayHelloResponseType;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import javax.xml.ws.BindingProvider;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
 
 public class HelloWorldServiceIT {
 
+    private static final String appUrl = System.getProperty("appServerUrl", "http://localhost:8080");
+    private static final String serviceEndpointUrl = appUrl + "/thirdparty-soap-service-test-support-war/HelloWorldService";
+    private static final String wsdlUrl = serviceEndpointUrl + "?wsdl";
+    private HelloWorldPortType helloWorldServiceProxy;
 
+    public HelloWorldServiceIT() {
+        helloWorldServiceProxy = new HelloWorldService().getHelloWorldPort();
+
+        BindingProvider bindingProvider = (BindingProvider) helloWorldServiceProxy;
+        bindingProvider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, serviceEndpointUrl);
+    }
+
+    @Test
+    @Ignore
+    public void shouldGetGreetingMessageFromThirdPartyService() {
+        SayHelloRequestType request = new SayHelloRequestType();
+        request.setName("Harish");
+        request.setGender("Male");
+        SayHelloResponseType response = helloWorldServiceProxy.sayHello(request);
+        String expectedMessage = String.format("Hi %1s, your gender is %2s",request.getName(), request.getGender());
+        assertEquals(expectedMessage, response.getGreetings());
+    }
 }
